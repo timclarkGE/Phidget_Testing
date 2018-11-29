5ce6883136c27247be0ad1e302219a160e7630bb
IVUT-DEV-001 <timothy.clark@ge.com>
2018-11-29 06:25:00 -0500

    Properly closed the telnet connection

--- Telnet_Client.py
+++ Telnet_Client.py
@@ -1,8 +1,17 @@
 import telnetlib
+import time
 
-HOST = "localhost"
+HOST = "192.168.1.100"
 #Connect us to the server
 tn = telnetlib.Telnet(HOST, "2217")
-
-tn.write("Hello World")
+text = "Hello World\n"
+iter = 5
+while iter:
+    tn.write(text.encode('ascii'))
+    time.sleep(1)
+    iter -= 1
+    print("here")
+text2 = "DONE"
+tn.write(text2.encode('ascii'))
+tn.close()
 print(tn.read_all())
